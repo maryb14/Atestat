a8d4924f3f915e7fd5214429efec33d071339946
marilena <marilena@marilena-eMachines-E525.(none)>
2016-03-31 08:25:40 +0300

    some changes

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ var app = angular.module('AIApp', ['ui.router']);
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   var home  = { name: 'home',  url: '/',       templateUrl: 'home.html'  };
   var istoric = {name: 'istoric', url: '/istoric', templateUrl: 'istoric.html'};
+  var knowmore = {name: 'knowmore', url: '/knowmore', templateUrl: 'knowmore.html'};
   $stateProvider.state(home);
   $stateProvider.state(istoric);
+  $stateProvider.state(knowmore);
   $urlRouterProvider.otherwise('/');
 }]);
 
@@ -24,6 +26,12 @@ app.controller('AIAppController', ['$scope', '$state', function($scope, $state) 
   }
   $scope.goToHomePage = goToHomePage;
 
+
+  function goToKnowmorePage(){
+    $state.go('knowmore');   
+  }
+
+  $scope.goToKnowmorePage = goToKnowmorePage;
   // end
 
   window.sc = $scope;
